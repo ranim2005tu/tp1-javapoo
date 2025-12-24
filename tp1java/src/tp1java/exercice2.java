4b8a4d3e0dc79d80b2c30b46694bf20aa2d08141
Ranim <ranimnasri35@gmail.com>
2025-12-24 09:26:06 +0100

    first commit exercice2

---
+++ tp1java/src/tp1java/exercice2.java
@@ -0,0 +1,14 @@
+package tp1java;
+
+public class exercice2 {
+
+	public static void main(String[] args) {
+		float moyenne= 10.5f;
+		System.out.println(25+25);
+		System.out.println("25"+25);
+		System.out.println("Mohamed"+1);
+		System.out.println("La moyenne "+"de la classe= "+moyenne+2);
+
+	}
+
+}
