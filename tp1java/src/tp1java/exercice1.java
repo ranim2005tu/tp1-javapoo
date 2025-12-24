b6795ca5066da7664045d1988a49744d78a8b587
Ranim <ranimnasri35@gmail.com>
2025-12-24 09:22:13 +0100

    first commit exercice1 tp1

---
+++ tp1java/src/tp1java/exercice1.java
@@ -0,0 +1,12 @@
+package tp1java;
+
+public class exercice1 {
+
+	public static void main(String[] args) {
+		System.out.println("Je suis un génie");
+		System.out.println();
+		System.out.println("Je sais écrire un programme 'JAVA'");
+
+	}
+
+}
